8d25e8adf66f52add14fd21ba86ece4a4fd7aa68
Mostafiz <mostafiz4372@gmail.com>
2026-05-15 16:53:09 +0600

    set deployment socket url in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://intro-socket-o3yg.vercel.app");
 
 function App() {
   const [message, setMessage] = useState("");
