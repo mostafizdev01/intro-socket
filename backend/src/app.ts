2504732a1b993ebb8be8d9244d3ceadb7bf2e9c5
Mostafiz <mostafiz4372@gmail.com>
2026-05-15 16:57:01 +0600

    added deployment url for cors issues

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json()); // Parse incoming JSON requests
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://intro-socket.vercel.app"],
     credentials: true,
   })
 );
