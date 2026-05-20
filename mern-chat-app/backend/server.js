cfff2ddb996b3f028090cfe6daad364923e94d37
Mostafiz <mostafiz4372@gmail.com>
2026-05-20 22:55:25 +0600

    solved the running server issues

--- mern-chat-app/backend/server.js
+++ mern-chat-app/backend/server.js
@@ -25,8 +25,13 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+app.get("/", (req, res) => {
+	res.json({
+		success: true,
+		status: 200,
+		message: "Server is running..."
+	})
+	// res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
